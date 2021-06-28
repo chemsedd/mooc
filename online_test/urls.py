645d3c1913530ff99605e488a6d3afb6bbc79311
Chems Eddine Senoussi <chemsedd@outlook.fr>
2021-06-28 14:51:59 +0100

    Added Post home page

--- online_test/urls.py
+++ online_test/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.uzls')),
+    path('blog/', include('blog.urls')),
     path('', include('quiz.urls'))
 ]
